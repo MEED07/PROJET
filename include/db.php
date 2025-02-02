0c6426e690ae152b41dc6fb60f773dfecae09a51
Amine <nacierabne65@gmail.com>
2025-02-02 18:02:15 +0100

    modification 3la l index.php

--- include/db.php
+++ include/db.php
@@ -1,2 +1,13 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=ecommerce','root','');
+$host = 'localhost:3308';
+$db = 'ecommerce';
+$user = 'root';
+$pass = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $db: " . $e->getMessage());
+}
+?>
